612e40b0c9a9f8bd9fb44a11ee70638d9110e307
mlpce <154090656+mlpce@users.noreply.github.com>
2024-09-01 18:26:30 +0100

    mlpce: In luaconf.h enable #define LUA_USE_C89

--- luaconf.h
+++ luaconf.h
@@ -41,7 +41,7 @@
 ** Define it if you want Lua to avoid the use of a few C99 features
 ** or Windows-specific features on Windows.
 */
-/* #define LUA_USE_C89 */
+#define LUA_USE_C89
 
 
 /*
@@ -795,7 +795,7 @@
 */
 
 #ifdef SLINPUT_ENABLED
- 
+
 #include "extend/extend.h"
 #define lua_initreadline(L)	(EXTEND_Init(L))
 #define lua_readline(L,b,p)	(EXTEND_Get(L, p, LUA_MAXINPUT, b))
